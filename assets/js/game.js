fdcd02c96366e0d0c62007e87372e17fc0abb89c
Jonathan Wilferd <jwilferd10@yahoo.com>
2020-10-22 14:10:32 -0700

    Feat: Stopping point, toyed around with js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,23 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+// This presents a string message
+
+console.log("This logs a string, good for leaving yourself a message");
+
+// this will do math and log 20
+
+console.log(10 + 10);
+
+// this is a string concatenation that connects string and variable
+
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+} 
+
+// fight();
+
