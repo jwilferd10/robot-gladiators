c073bae1ab9a19d8a0f3cc091f8d2761301b74e4
Jonathan Wilferd <jwilferd10@yahoo.com>
2020-10-24 00:00:54 -0700

    Feat: Multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,10 +13,10 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-// NOTE: In school example uses enemyName, using that creates issues. I've gone with enemyNames.
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
-      // ask player if they'd liked to fight or run
+              
+        // ask player if they'd liked to fight or run
       var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
   
       // if player picks "skip" confirm and then stop the loop
@@ -71,8 +71,26 @@ var fight = function(enemyName) {
   };
 
 // Repeat and execute as long as the enemy-robot is alive 
+
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
-}  
+    if (playerHealth > 0) {
+        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+    
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+    
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+    
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+    
+    }  else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
+};
